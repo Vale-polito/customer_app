b8c5f56cc1f60e8691cff68a3c3e00ab0b75d3a3
Mina <minaa.aghaeii@gmail.com>
2019-04-23 17:23:58 +0200

    edited

--- app/src/main/java/com/example/customer/ApplyHelpActivity.java
+++ app/src/main/java/com/example/customer/ApplyHelpActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class ApplyHelpActivity extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -30,5 +32,14 @@ public class ApplyHelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_help);
+
+        Button btnApplyNow=findViewById(R.id.btnApplyNow);
+        btnApplyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplyHelpActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
